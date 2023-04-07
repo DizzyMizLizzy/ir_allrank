36758eed2eb05fa335e0ca3be245bbd563433a64
Yimin ZHOU <55327029+Yimin-zhou@users.noreply.github.com>
2023-04-07 23:36:23 +0200

    add rankenet weighted results

--- test_all.py
+++ test_all.py
@@ -1,17 +1,17 @@
 import os
 import subprocess
 
-folders = ['pointwise', 'pairwise', 'listwise']
+folders = ['listwise']
 folds = [1, 2, 3, 4, 5]
-configs = ['pointwise_mse.json', 'pairwise_ranknet.json', 'listwise_approxNDCG.json', 'listwise_neuralNDCG.json']
+configs = ['listwise_approxNDCG.json']
 
 for folder in folders:
     for fold in folds:
         for config in configs:
-            config_path = f'configs\\{folder}\\fold{fold}\\{config}'
-            run_id = 'test_1'
-            job_dir = f'test_WeightedRanknet\{folder}\\{config}\\fold{fold}'
-            model_dir = f'D:\\Programming\\Assignments\\IR\\project\\run\\ir_allrank\\allrankrun_MSLR-WEB10K\\{folder}\\{config}\\fold{fold}\\results\\run_1\\model.pkl'
+            config_path = f'configs_2\\{folder}\\fold{fold}\\{config}'
+            run_id = 'test_appoxNDCG'
+            job_dir = f'test_mq2008\{folder}\\{config}\\fold{fold}'
+            model_dir = f'D:\\Programming\\Assignments\\IR\\project\\run\\ir_allrank\\allrankrun_MQ2008\\{folder}\\{config}\\fold{fold}\\results\\run_1\\model.pkl'
 
             if os.path.exists(config_path):
                 print(f'Testing {config} for fold {fold}...')
